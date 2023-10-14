ba07cbd5552029b1447fec19a3ac6bfa6596f2e1
Ruchita613 <ruchita.sawale@getpeppermint.co>
2023-10-14 16:43:45 +0530

    update on Hire Me button

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,6 +3,7 @@ import '../Intro/intro.css'
 import bg from '../../assets/image.png'
 import btnImg from '../../assets/hireme.png'
 import {Link} from 'react-scroll'
+import ResumePDF from '../../assets/resume.pdf'
 function Intro() {
     return (
         <section id='intro'>
@@ -10,7 +11,8 @@ function Intro() {
                 <span className='hello'> Hello</span>
                 <span className='introText'> I'am <span className='introName'> Ruchita </span> <span className='animated-text'><br/>Software Developer</span></span>
                 <p className='introPara'> I am a skilled software Developer.<br/> With experience in creating beautiful front end. </p>
-                <Link><button className='btn'><img src={btnImg} alt='Hireme' className='btnImg'/> Hire me </button></Link>
+                <a href= {ResumePDF} download={"Ruchita Resume Copy "}  target="_blank"
+        rel="noreferrer"> <button className='btn'><img src={btnImg} alt='Hireme' className='btnImg'/> Hire me </button></a>
             </div>
             <img src={bg} alt='Profile ' className='bg'/>
         </section>
